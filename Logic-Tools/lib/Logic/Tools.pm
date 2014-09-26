cc9ffd068a679e5984f885cf489b9150613a3b23
lagutas <root@t-1.fr.sipdc.priv>
2014-09-26 12:08:19 +0300

    add feature, logsize and lognum for autorotation. see documentation.

--- Logic-Tools/lib/Logic/Tools.pm
+++ Logic-Tools/lib/Logic/Tools.pm
@@ -4,6 +4,8 @@ use 5.10.1;
 use strict;
 use warnings;
 use Config::IniFiles;
+use File::stat;
+use Archive::Tar;
 #use Time::localtime;
 
 use POSIX;
@@ -14,12 +16,12 @@ Voiecng::Tools - The great new Logic::Tools!
 
 =head1 VERSION
 
-Version 0.6
+Version 0.4.2
 
 =cut
 
 my @ISA = qw(Logic);
-our $VERSION = '0.06';
+our $VERSION = '0.4.2';
 
 
 =head1 SYNOPSIS
@@ -46,6 +48,12 @@ if you don't export anything, such as for a purely object-oriented module.
 
 sub new
 {
+    $SIG{INT} = \&close_prog;
+    $SIG{QUIT} = \&close_prog;
+    $SIG{TERM} = \&close_prog;
+
+    $SIG{CHLD} = 'IGNORE';
+
     my $invocant = shift; # первый параметр - ссылка на объект или имя класса
     my $class = ref($invocant) || $invocant; # получение имени класса        
     my $self = { @_ }; # ссылка на анонимный хеш - это и будет нашим новым объектом, инициализация объекта
@@ -91,7 +99,7 @@ sub check_proc
     if( -e $pid_f ) 
     {
 
-        open(my $pid_file,'<',$pid_f) || die "[FAILED] Не удалось открыть файл $pid_f";
+        open(my $pid_file,'<',$pid_f) || die "[FAILED] can't open $pid_f";
         my $pid=<$pid_file>;
         close $pid_file;
         chomp $pid;
@@ -100,12 +108,12 @@ sub check_proc
         unless( -e "/proc/$pid" )
         {
             #print STDERR "Файл блокировки уже существует, но демон с pid=$pid не существует\n";
-            die "[FAILED] Не удается удалить файл блокировки $pid_f\n" if ( !unlink $pid_f );
+            die "[FAILED] can't delete file $pid_f\n" if ( !unlink $pid_f );
             #print STDERR "Файл блокировки удален\n";
         }
         else
         {
-            die "Процесс уже запущен. Процесс с pid=$pid\n";
+            die "process alredy run pid=$pid\n";
         }
     }
     return 1;
@@ -120,11 +128,89 @@ sub logprint
     my $loglevel=shift;
     my $message=shift;
 
-    #my $tm=localtime;
-
     my ($sec, $min, $hour, $day, $mon, $year) = ( localtime(time) )[0,1,2,3,4,5];
+    
+    #высчитывае максимальный размер лога в байтах
+    if(defined($self->{'logsize'}))
+    {
+        my $logsize;
+        my $lognum;
 
-    open my $logfile,">>",$self->{'logfile'};    
+        if(!defined($self->{'log_num'}))
+        {
+            $lognum=1;
+        }
+        else
+        {
+            $lognum=$self->{'log_num'};
+        }
+        
+        if($self->{'logsize'}=~/^(\d+)(.{2})$/)
+        {
+            if(($2 eq "Kb")||($2 eq "KB")||($2 eq "kb"))
+            {
+                $logsize=$1*1024;
+            }
+            elsif(($2 eq "Mb")||($2 eq "MB")||($2 eq "mb"))
+            {
+                $logsize=$1*1024*1024;
+            }
+            elsif(($2 eq "Gb")||($2 eq "GB")||($2 eq "gb"))
+            {
+                $logsize=$1*1024*1024*1024;
+            }
+        }
+        else
+        {
+            $logsize=$self->{'logsize'};
+        }
+
+        my $statfile = stat($self->{'logfile'}) or die "печалька\n";
+        my $size = $statfile->size;
+
+
+        if($size>$logsize)
+        {
+            my $filename;
+            my $log_path;
+            if($self->{'logfile'}=~/^(.+)\/(.+).log$/)
+            {
+                $log_path=$1;
+                $filename=$2;
+            }
+    
+            #проверяем количество файлов которые уже есть в логах
+            my @gz_files_list = glob($log_path.'/'.$filename.'*.gz');
+
+            my $log_file_exist=scalar(@gz_files_list);
+
+            #количество лишних файлов
+            my $num_of_redundant_files;
+
+            if($log_file_exist>=$lognum)
+            {
+                $num_of_redundant_files=$log_file_exist-$lognum;
+                for(my $i=0;$i<=$num_of_redundant_files;$i++)
+                {
+                    unlink($gz_files_list[$i]);
+                }
+            }
+
+            
+
+
+            my $tar = Archive::Tar->new;
+            $tar->add_files($self->{'logfile'});
+            #формируем суффикс чтобы
+            my $suffix=sprintf("%04d%02d%02d%02d%02d%02d",$year+1900,$mon+1,$day,$hour,$min,$sec);
+            $tar->write('/var/log/'.$filename.'-'.$suffix.'.gz', COMPRESS_GZIP);
+            #удаляем лог
+            unlink($self->{'logfile'});
+        }
+    }
+    
+
+    open my $logfile,">>",$self->{'logfile'};  
 
     printf $logfile ("%04d/%02d/%02d %02d:%02d:%02d [%d] %s: %s\n",$year+1900,$mon+1,$day,$hour,$min,$sec,$$,$loglevel,$message);
 
@@ -173,6 +259,66 @@ sub start_daemon
     $> = $uid;
 
     return 1;
+}
+
+
+#старт демона супервизором
+#первый порожденный пид
+my $first_child_pid=0;
+sub supervisor_start_daemon
+{
+    my $model=shift;
+    my $self=$model->new(%$model,@_);
+
+    my $runas_user=$self->{'runas_user'};
+    my $lock_file=$self->{'lock_file'};
+
+    my ($name, $passwd, $uid, $gid) = getpwnam($runas_user) or die "[FAILED] can't start under the $runas_user";
+    
+    $first_child_pid = fork();
+    
+    die "[FAILED] can't create fork: $!" unless(defined($first_child_pid));
+    
+     
+    if($first_child_pid)
+    {
+        # Запись файле блокировки
+        open(my $pid_file, ">" ,$lock_file) || die "[FAILED] can't create block file $lock_file\n";
+        print $pid_file "$first_child_pid";
+        close $pid_file;		
+        chown $uid, $gid, $lock_file;
+        while(1)
+        {
+            # Процесс запущен, но активного процесса с указанным PID нет
+
+            unless( -e "/proc/$first_child_pid" )
+            {
+                die "child $first_child_pid dead, exit\n";
+                exit;
+            }
+            sleep(1);
+        }
+    } 
+    else
+    {
+        # daemon
+        setpgrp();
+        select(STDERR); $| = 1;
+        select(STDOUT); $| = 1;
+    }
+
+    # Сброс привилегий
+    setuid($uid);
+    $< = $uid;
+    $> = $uid;
+}
+
+sub close_prog 
+{   
+	#отправка сигнала завершения дочернему процессу
+	kill("TERM",$first_child_pid);
+	die "TERM signal recieved\n";
+    exit;
 }
 
 =head1 AUTHOR
